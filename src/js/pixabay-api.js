bbcedb7cb80f174e3f2761fb737c514db69d34e7
Yilchik <yulyayulia2@gmail.com>
2024-04-08 22:01:18 +0200

    hw11 error2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,12 @@
 const API_KEY = '43275871-c76d4a7895f35b3cf58095282';
+const BASE_URL = 'https://pixabay.com/api';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import { createGallery } from './render-functions';
 
-export function fetchImages(text) {
+export async function fetchImages(text) {
   const params = new URLSearchParams({
     key: API_KEY,
     q: text,
@@ -14,7 +15,9 @@ export function fetchImages(text) {
     safesearch: 'true',
   });
 
-  fetch(`https://pixabay.com/api?${params}`)
+  const url = `${BASE_URL}?${params}`;
+
+  fetch(url)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
